5b5a43e69b4dee40a33df787240888fec6b4bbeb
Chris Nippard <chrisnippard@live.com>
2022-08-03 16:10:52 -0230

    LessThan portion of coding challenge for Esri

---
+++ LessThan/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LessThan
+{
+    internal class Program
+    {
+    
+        public static int getLessThanCount(int[] sortedArray, int lessThan)
+        {
+            try
+            {
+                if (sortedArray.Length == 0)
+                    return 0;
+
+                List<int> sortedList = sortedArray.ToList();
+
+                for (int i = 0; i < sortedList.Count; i++)
+                {
+                    if (sortedList[i] >= lessThan)
+                    {
+                        sortedList.Remove(sortedList[i]);
+                        i = i - 1;
+                    }
+                    else if (sortedList[i] == sortedList[i + 1])
+                    {
+                        sortedList.Remove(sortedList[i]);
+                        i = i - 1;
+                    }
+                }
+
+                return sortedList.Count;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught the following Exception: " + e.Message);
+                Console.ReadKey();
+                return -1;
+            }
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The total count is: {0}", getLessThanCount(new int[] { 1, 2, 2, 3, 3, 4, 4, 4, 5}, 4));
+            Console.ReadKey();
+        }
+    }
+}
